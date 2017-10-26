55305981511fffa15fbd0a74137edaf53d3cd7d4
Mahesh Ambig <mahesh.ambiga@gmail.com>
2017-10-26 19:08:08 +0530

    updated audio path

--- src/client/components/ViewStory/index.js
+++ src/client/components/ViewStory/index.js
@@ -45,13 +45,13 @@ class ViewStory extends Component {
                   </div>
                 </div>
                 <AudioPlayer
-                  playlist={[{ url: `${GET_AUDIO_FILE_API}/${result.data[0].story.path}`,
+                  playlist={[{ url: `https://storage.googleapis.com/story_audios_bucket/${result.data[0].story.path}`,
                   displayText: `A story by ${result.data[0].createdBy}` }]}
                              hideBackSkip={true}
                              autoplay={true}
                              autoplayDelayInSeconds={2}
-                             hideForwardSkip={true} c
-                             ycle={false} />
+                             hideForwardSkip={true}
+                             cycle={false} />
               </div>}
             </section>
           </div>
