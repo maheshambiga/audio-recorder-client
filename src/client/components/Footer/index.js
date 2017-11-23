801800966716bf8c61e0821a00a343e34b1af886
Mahesh Ambig <mambig@spient.com>
2017-11-23 15:10:50 +0530

    updated contact email

--- src/client/components/Footer/index.js
+++ src/client/components/Footer/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => (<footer className="appFooter color_FFF ">
   <p>&copy; 2017 - All rights reserved</p>
   <p>
-    <a className="textUnderline color_747676" href="mailto:dhanya.sada@gmail.com">contact</a>
+    <a className="textUnderline color_747676" href="mailto:mahesh.ambiga@gmail.com">contact</a>
   </p>
 </footer>);
 
